0f84261be0206e61e2db56817d60a34407d8cb37
jan-simacek <simacek.jan@gmail.com>
2019-01-30 18:06:46 +0100

    colors fixing

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -10,8 +10,8 @@ export class Loader extends Component<LoaderProps> {
     public render(): ReactNode {
         return (
             this.props.isSpan ? 
-                <span key='loader' className='loader-wrapper'><CircularProgress className='loader' /></span> :
-                <div key='loader' className='loader-wrapper'><CircularProgress className='loader' /></div>
+                <span key='loader' className='loader-wrapper'><CircularProgress className='loader' color="secondary" /></span> :
+                <div key='loader' className='loader-wrapper'><CircularProgress className='loader' color="secondary" /></div>
         )
     }
 }
